75820fd10e850b8910be3018925f0feabf1d4a5b
phillyai <sonagi.philly@gmail.com>
2017-12-20 18:57:12 +0900

    Span Extension Method 사용해서 코드 정리

--- X86Sharp.Tests/VMTests.cs
+++ X86Sharp.Tests/VMTests.cs
@@ -48,11 +48,11 @@ namespace X86Sharp.Tests
             {
                 var dword0to4 = vm.Memory.GetValue(new Address(displacement: 0), 4);
                 var mov = vm.Instructions.GetInstructionFromType<InstructionCallback2args>(InstructionType.Mov);
-                ref var refptr = ref Unsafe.AsRef<uint>(Unsafe.AsPointer(ref dword0to4[0]));
+                ref var refptr = ref dword0to4.NonPortableCast<byte, uint>()[0];
                 uint num = 0x12345678;
                 mov(ref refptr, ref num);
 
-                var res = BitConverter.ToUInt32(new byte[4] { dword0to4[0], dword0to4[1], dword0to4[2], dword0to4[3] }, 0);
+                var res = BitConverter.ToUInt32(dword0to4.ToArray(), 0);
                 Assert.AreEqual(num, res);
 
                 System.Diagnostics.Debugger.Break();
