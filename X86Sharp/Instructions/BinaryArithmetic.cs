4bca191e83145840ed97d5d292efdbacd20948e8
phillyai <sonagi.philly@gmail.com>
2017-12-26 12:00:49 +0900

    Add 구현

--- X86Sharp/Instructions/BinaryArithmetic.cs
+++ X86Sharp/Instructions/BinaryArithmetic.cs
@@ -2,5 +2,26 @@
 {
     public partial class VM
     {
+        [Instruction(InstructionType.ADD, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05,
+            0x8000, 0x8100, 0x8300)]
+        public void Add(ref uint dest, ref uint source)
+        {
+            dest += source;
+
+            Eflags.SF = (dest & 0x80000000) == 0x80000000;
+            Eflags.ZF = dest == 0;
+            Eflags.CF = dest < source;
+            Eflags.PF = (((((byte)dest * 0x0101010101010101UL) & 0x8040201008040201UL) % 0x1FF) & 1) == 0;
+
+            Eflags.OF = false;
+            try
+            {
+                var res = checked(dest + source);
+            }
+            catch (System.OverflowException)
+            {
+                Eflags.OF = true;
+            }
+        }
     }
 }
